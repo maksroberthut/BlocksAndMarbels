103e67c0ec955fd28a467daefd01db9789a4d0b9
maksroberthut <maksroberthut@googlemail.com>
2019-05-08 12:11:27 +0200

    Initial commit

---
+++ src/Physic/Physics.java
@@ -0,0 +1,74 @@
+package Physic;
+
+/**
+ * Interface that defines the functions for the physics lopp that gets calculated every Frame
+ */
+public interface Physics {
+
+    //Constants
+    static final double gravity = 9.8;//Gravitational Force downwards
+
+
+    /**
+     * Function to Calculate the velocity
+     * @param firstPosition
+     * @param secondPosition
+     * @param firstFrame
+     * @param secondFrame
+     * @return velocity
+     */
+    static  double calculateVelocity(int firstPosition, int secondPosition, int firstFrame, int secondFrame){
+
+        int deltaposition = secondPosition - firstPosition;
+        int deltaframe = secondFrame - firstFrame;
+
+        int vel = deltaposition / deltaframe;
+        return vel;
+
+    }
+
+    /**
+     * Function to Calculate acceleration
+     * @param firtsVel
+     * @param secondVel
+     * @param frame
+     * @return accel
+     */
+    static double calculateAcceleration(double firtsVel, double secondVel, double frame){
+
+        double deltaVel = secondVel - firtsVel;
+        double accel = deltaVel / frame;
+
+        return accel;
+
+    }
+
+    /**
+     * Function to calculate the kinetic Force
+     * @param mass
+     * @param vel
+     * @return kineticForce
+     */
+    static double calculateKinetic(double mass, int vel){
+
+        double kineticForce = 0.5 * mass * (vel*vel);
+        return  kineticForce;
+
+    }
+
+
+    /**
+     * Function to calculate the potential Force
+     * @param mass
+     * @param height
+     * @return potentialForce
+     */
+    static double calculatePotetial(double mass, double height){
+
+        double potentialForce = mass * gravity * height;
+        return potentialForce;
+
+    }
+
+
+}
