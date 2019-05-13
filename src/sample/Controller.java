3b911cba21466b13966012d32c0975eea0ca7ab1
Rumeysa93 <rumeysa93@live.de>
2019-05-13 13:38:58 +0200

    Ball fällt

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -19,13 +19,45 @@ public class Controller {
     public GridPane canvas;
     public Label ballSteht;
     public Circle kugel;
-
+    double newX,newY;
 
     public void starteSpiel(ActionEvent actionEvent) {
-
+        //hier greift das Programm auf die Zeit zu
+        System.out.println("Started");
+        //https://www.programcreek.com/java-api-examples/?api=javafx.animation.AnimationTimer
+        //60 fps
+        new AnimationTimer() {
+            public void handle(long currentNanoTime) {
+                // calculate time since last update.
+                moveBall();
+            }
+        }.start();                                                                  // Was diese?
 
 
     }
+
+    //geschwindigkeit und beschleuniguf wurden initialisiert
+    double geschw;
+    double beschl = 9.81*1/60;
+
+    public void moveBall(){
+        // formel für die geschwindigkeit
+        geschw = geschw + beschl;
+
+        //kugel soll für jeden Frame eine neue Position annehmen
+        newX = kugel.getCenterX();
+        newY = kugel.getCenterY() + geschw;
+
+        kugel.setCenterX(newX);
+        kugel.setCenterY(newY);
+
+        System.out.println("geschw: " + geschw);
+
+
+       /* double finalY=580;
+        ball1.setLayoutY(finalY - ball1.getLayoutBounds().getMinY());*/
+    }
+
 
     public void stoppeBall(ActionEvent actionEvent) {
 
