e45a33765599993ab624edd890cca2f80c28baf0
Rumeysa93 <rumeysa93@live.de>
2019-05-13 13:25:45 +0200

    GUI erstellt PLAY STOP RESET verknüpft(leere Methoden) Bild eingefügt

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,37 @@
 package sample;
 
+import javafx.animation.AnimationTimer;
+import javafx.animation.KeyFrame;
+import javafx.animation.KeyValue;
+import javafx.animation.Timeline;
+import javafx.event.ActionEvent;
+import javafx.geometry.Bounds;
+import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.GridPane;
+import javafx.scene.shape.Circle;
+import javafx.util.Duration;
+
 public class Controller {
+
+
+    public Label ballRollt;
+    public GridPane canvas;
+    public Label ballSteht;
+    public Circle kugel;
+
+
+    public void starteSpiel(ActionEvent actionEvent) {
+
+
+
+    }
+
+    public void stoppeBall(ActionEvent actionEvent) {
+
+    }
+
+    public void vonVorne(ActionEvent actionEvent) {
+
+    }
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -2,17 +2,22 @@ package sample;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
+
+import java.awt.*;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setTitle("Yeas Game");
+        primaryStage.setScene(new Scene(root, 1000, 1000));
         primaryStage.show();
     }
 
