447f5cda50a1940930f1e2f3f64439ad03e26093
Rumeysa93 <rumeysa93@live.de>
2019-05-20 12:49:51 +0200

    Bilder sind Drin

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -10,7 +10,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("NewTryFXML2.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("NewTryFXML3.fxml"));
         primaryStage.setTitle("Blocks and Marbels");
         primaryStage.setScene(new Scene(root, 1200, 1000));
         primaryStage.show();
